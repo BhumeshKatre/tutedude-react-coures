e2029ad588529b399dedd5fc30249303af70f04b
DevDynamoWeb <dev.bhumesh@gmail.com>
2025-08-14 07:24:05 +0530

    add Title component and integrate it into Assignment1Counter & done

--- src/assignhments/FunctionAssignments.jsx/.qodo/Assignment_1Counter.jsx
+++ src/assignhments/FunctionAssignments.jsx/.qodo/Assignment_1Counter.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Title from '../../../Components/Title';
 
 const Assignment1Counter = () => {
   const [addNumber, setAddNumber] = useState(0);
@@ -14,14 +15,20 @@ const Assignment1Counter = () => {
   }
 
   return (
-    <div className='flex justify-center items-center h-screen w-screen gap'>
-
-      <button
-        className='bg-red-400 text-white font-bold text-2xl rounded-tl-2xl rounded-bl-2xl px-5' onClick={handleMinussBtn}>-</button>
-      <p className='px-5 '>{addNumber}</p>
-      <button
-        className='bg-green-400 text-white font-bold text-2xl rounded-tr-2xl rounded-br-2xl px-5'
-        onClick={handlePlusBtn}>+</button>
+    <div className='flex justify-center items-center h-screen w-screen  '>
+      <div className="flex flex-col gap-10 ">
+        <div className='w-full text-center text-2xl  font-bold'>
+          <Title Title={'Counter App'} />
+        </div>
+        <div className='flex items-center w-full border-2 border-gray-400 rounded-3xl '>
+          <button
+            className='bg-red-400 p-0 text-white font-bold text-2xl rounded-tl-2xl rounded-bl-2xl px-5' onClick={handleMinussBtn}>-</button>
+          <p className='px-5 flex items-center'>{addNumber}</p>
+          <button
+            className='bg-green-400 text-white font-bold text-2xl rounded-tr-2xl rounded-br-2xl px-5'
+            onClick={handlePlusBtn}>+</button>
+      </div>
+    </div>
 
     </div>
   );
